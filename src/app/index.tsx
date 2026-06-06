33a8ab2886b224158b755a9af1059858c12d098d
Syed Mashiur Rahman <syedshaon99@gmail.com>
2026-06-07 00:34:32 +0600

    shouldPlayInBackground added

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -48,6 +48,7 @@ export default function App() {
       try {
         await setAudioModeAsync({
           playsInSilentMode: true,
+          shouldPlayInBackground: true,
           interruptionMode: "doNotMix", // Prevents Android system termination hooks
           allowsRecording: false,
         });
